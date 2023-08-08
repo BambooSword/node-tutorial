3a1239327dc6d9560192d998b4d6f0608aa36567
BambooSword <herodreamdeng@gmail.com>
2023-08-08 18:02:52 +0800

    feat: learn http

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('hello node, I am coming');
+// import { URL } from 'url'
+import http from 'node:http'
+
+// console.log(
+//   '🚀 ~ file: app.ts:5 ~ import.meta.url:',
+//   new URL('', import.meta.url)
+// )
+// console.log(
+//   '🚀 ~ file: app.ts:5 ~ import.meta.url:',
+//   new URL('.', import.meta.url)
+// )
+function rqListener(req: http.IncomingMessage, res: http.ServerResponse) {
+  console.log('🚀 ~ file: app.ts:13 ~ rqListener ~ req:', req)
+}
+const server = http.createServer(rqListener)
+
+server.listen(8080)
